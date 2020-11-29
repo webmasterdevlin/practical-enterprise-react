e5c32a605209af9082497f83b6c2259132a00a23
Devlin Duldulao <webmasterdevlin@gmail.com>
2020-11-29 18:05:46 +0100

    updated

--- chapter-5/src/app/containers/HomePage/index.tsx
+++ chapter-5/src/app/containers/HomePage/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+  }),
+);
 
 export function HomePage() {
+  const classes = useStyles();
+
   return (
-    <>
-      <Helmet>
-        <title>Home Page</title>
-        <meta name="description" content="A Boilerplate application homepage" />
-      </Helmet>
-      <span>HomePage container</span>
-    </>
+    <div className={classes.root}>
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+    </div>
   );
 }
