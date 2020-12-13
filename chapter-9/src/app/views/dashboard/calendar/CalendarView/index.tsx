396a572c887d22828fd2497bb0e42e6a6e8fc3af
Devlin Duldulao <webmasterdevlin@gmail.com>
2020-12-13 14:32:14 +0100

    updated

--- chapter-9/src/app/views/dashboard/calendar/CalendarView/index.tsx
+++ chapter-9/src/app/views/dashboard/calendar/CalendarView/index.tsx
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react';
-import { Container, Dialog, makeStyles } from '@material-ui/core';
+import React, { useEffect, useRef, useState } from 'react';
+import moment from 'moment';
+import FullCalendar from '@fullcalendar/react';
+import {
+  Container,
+  Dialog,
+  makeStyles,
+  useMediaQuery,
+} from '@material-ui/core';
 import Page from 'app/components/page';
-import { EventType } from 'models/calendar-type';
+import { EventType, ViewType } from 'models/calendar-type';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store/reducers';
 import Header from './Header';
+import Toolbar from './Toolbar';
 import {
   closeModal,
   getEvents,
@@ -20,6 +28,16 @@ const CalendarView = () => {
   );
   const selectedEvent = useSelector(selectedEventSelector);
 
+  const mobileDevice = useMediaQuery('(max-width:600px)');
+  const [date, setDate] = useState<Date>(moment().toDate());
+  const [view, setView] = useState<ViewType>(
+    mobileDevice ? 'listWeek' : 'dayGridMonth',
+  );
+
+  /* useRef is used to access DOM elements and 
+  to persist state or values in successive or next renders */
+  const calendarRef = useRef<FullCalendar | null>(null);
+
   useEffect(() => {
     dispatch(getEvents());
   }, []);
@@ -32,11 +50,62 @@ const CalendarView = () => {
     dispatch(closeModal());
   };
 
+  const handleDateNext = (): void => {
+    const calendarEl = calendarRef.current;
+
+    if (calendarEl) {
+      const calendarApi = calendarEl.getApi();
+
+      calendarApi.next();
+      setDate(calendarApi.getDate());
+    }
+  };
+
+  const handleDatePrev = (): void => {
+    const calendarEl = calendarRef.current;
+
+    if (calendarEl) {
+      const calendarApi = calendarEl.getApi();
+
+      calendarApi.prev();
+      setDate(calendarApi.getDate());
+    }
+  };
+
+  const handleDateToday = (): void => {
+    const calendarEl = calendarRef.current;
+
+    if (calendarEl) {
+      const calendarApi = calendarEl.getApi();
+
+      calendarApi.today();
+      setDate(calendarApi.getDate());
+    }
+  };
+
+  const handleViewChange = (newView: ViewType): void => {
+    const calendarEl = calendarRef.current;
+
+    if (calendarEl) {
+      const calendarApi = calendarEl.getApi();
+
+      calendarApi.changeView(newView);
+      setView(newView);
+    }
+  };
+
   return (
     <Page className={classes.root} title="Calendar">
       <Container maxWidth={false}>
         <Header onAddClick={handleAddClick} />
-
+        <Toolbar
+          date={date}
+          onDateNext={handleDateNext}
+          onDatePrev={handleDatePrev}
+          onDateToday={handleDateToday}
+          onViewChange={handleViewChange}
+          view={view}
+        />
         <Dialog
           maxWidth="sm"
           fullWidth
