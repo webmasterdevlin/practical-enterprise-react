88b0920058250ad20927efe411a79e4e311cbf10
Devlin Duldulao <webmasterdevlin@gmail.com>
2020-12-21 14:26:20 +0100

    updated

--- chapter-12/src/app/layouts/main-layout/index.tsx
+++ chapter-12/src/app/layouts/main-layout/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import type { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import { makeStyles } from '@material-ui/core';
 
 import NavigationBar from './navigation-bar';
